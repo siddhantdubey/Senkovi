3402773588fc4366bcd6429c3d3f654bed1207ac
siddhantdubey <sid12.dubey@gmail.com>
2023-03-18 15:49:58 -0400

    renamed files and such

--- buggy_program.py
+++ buggy_program.py
@@ -3,6 +3,6 @@ def buggy_function():
     number2 = 5
     number1 = "4"
     result = number1 + number2
-    print(f"Sum: {result}")
+    return result
 if __name__ == "__main__":
-    buggy_function()
+    print(buggy_function())
